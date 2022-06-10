80f52f350052760353414b2a83ec0b2673258295
gregoindev <gregocodes23@gmail.com>
2022-06-10 14:50:29 -0700

    Scrimba solo project

---
+++ index.js
@@ -0,0 +1,114 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let pwBoxOne = document.querySelector(".pw-box-1");
+let pwBoxTwo = document.querySelector(".pw-box-2");
+
+function randomChar() {
+  let char = Math.floor(Math.random() * characters.length);
+  return characters[char];
+}
+
+function generate() {
+  let passwords = ["", ""];
+
+  for (let i = 1; i < 15; i++) {
+    passwords[0] += randomChar();
+    passwords[1] += randomChar();
+  }
+
+  pwBoxOne.textContent = passwords[0];
+  pwBoxTwo.textContent = passwords[1];
+  return passwords;
+}
